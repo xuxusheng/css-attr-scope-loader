b8a17bf7d8f349aa850df0c8b96abbe8530b11e8
xusheng-antiy <xusheng@antiy.cn>
2016-08-09 15:01:16 +0800

    发布成依赖包之后，应该直接抛出函数了，而不再是路径

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-module.exports = loader
-
-// 返回 loader.js 的绝对路径
-function loader() {
-    return [
-        require.resolve('./lib/loader.js')
-    ].join('!')
-}
+module.exports = require('./lib/loader')
